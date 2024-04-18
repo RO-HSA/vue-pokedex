6193f29f01b16581e49cc8bfd846291e0b5ae794
Robert Santos <robertdahora@gmail.com>
2024-04-18 17:33:47 -0300

    Settin up Bootstrap Vue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import Vue, { createApp } from '@vue/compat'
 import App from './App.vue'
+import { BootstrapVue } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 
 createApp(App).mount('#app')
